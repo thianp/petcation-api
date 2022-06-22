98200a771d2e849cc3b17b4f0fd31fd31424713b
Khanin <KhaninDamrong@gmail.com>
2022-06-22 16:40:04 +0700

    model-geography-finish

---
+++ models/SubDistrict.js
@@ -0,0 +1,35 @@
+module.exports = (sequelize, DataTypes) => {
+  const SubDistrict = sequelize.define(
+    'SubDistrict',
+    {
+      zipCode: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      nameTh: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      nameEn: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    {
+      underscored: true,
+    }
+  );
+
+  SubDistrict.associate = (models) => {
+    SubDistrict.belongsTo(models.District, {
+      foreignKey: {
+        name: 'districtId',
+        allowNull: false,
+      },
+      onUpdate: 'RESTRICT',
+      onDelete: 'RESTRICT',
+    });
+  };
+
+  return SubDistrict;
+};
