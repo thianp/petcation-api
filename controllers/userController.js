3be91cbba42e5dd4d3c164cff2e28c29f93704d1
amorntep <amorntep.mechaniceng@gmail.com>
2022-06-27 17:50:26 +0700

    add-api-createuser

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 const { User } = require("../models");
 const createError = require("../utils/createError.js");
+const cloudinary = require("../utils/cloudinary");
 
 exports.getUser = async (req, res, next) => {
   try {
@@ -16,6 +17,43 @@ exports.getUser = async (req, res, next) => {
     res.status(200).json({
       user,
     });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.createProfileUser = async (req, res, next) => {
+  try {
+    const {
+      firstName,
+      lastName,
+      phoneNumber,
+      province,
+      district,
+      subDistrict,
+      zipCode,
+      address,
+    } = req.body;
+
+    let image;
+    if (req.file) {
+      const result = await cloudinary.upload(req.file.path);
+      image = result.secure_url;
+    }
+
+    const profileUser = await Product.findOne({
+      firstName,
+      lastName,
+      phoneNumber,
+      province,
+      district,
+      subDistrict,
+      zipCode,
+      address,
+      image,
+    });
+
+    res.status(201).json({ profileUser });
   } catch (err) {
     next(err);
   }
