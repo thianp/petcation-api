8ac4d9b4d1edd029cfac259de4f02370ad91fb70
thianp <pthpatchara@gmail.com>
2022-07-01 03:07:05 +0700

    added createBooking api

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -2,39 +2,152 @@ const omise = require("omise")({
   secretKey: "skey_test_5s9r39mkp5631hvcqa4",
   omiseVersion: "2019-05-29",
 });
-const { Booking, House } = require("../models");
+const {
+  Booking,
+  House,
+  sequelize,
+  Pet,
+  Bookingpet,
+  Bookinghouse,
+  User,
+  Host,
+  Bookingcustomer,
+} = require("../models");
 const createError = require("../utils/createError.js");
 
 exports.createBooking = async (req, res, next) => {
   try {
-    // const { id } = req.user;
-    // const {
-    //   checkInDate,
-    //   checkOutDate,
-    //   houseId,
-    //   price,
-    //   foodPrice,
-    //   serviceFee,
-    //   includeFood,
-    //   status,
-    //   paymentId,
-    // } = req.body;
+    const result = await sequelize.transaction(async (t) => {
+      const { id } = req.user;
+      const {
+        token,
+        checkInDate,
+        checkOutDate,
+        houseId,
+        price,
+        includeFood,
+        serviceFee,
+        foodPrice,
+        petIds,
+      } = req.body;
 
-    const { token } = req.body;
-    omise.charges.create(
-      {
-        amount: 100000,
-        currency: "thb",
-        card: token,
-      },
-      function (err, charge) {
-        /* Response. */
-        if (err) {
-          next(err);
+      // create charge (payment)
+      let createdCharge;
+      await omise.charges.create(
+        {
+          amount: 100000,
+          currency: "thb",
+          card: token,
+        },
+        function (err, charge) {
+          if (err) {
+            next(err);
+          }
+          createdCharge = charge;
         }
-        console.log(charge);
+      );
+
+      // create booking
+      const booking = await Booking.create(
+        {
+          checkInDate,
+          checkOutDate,
+          houseId,
+          price,
+          foodPrice,
+          serviceFee,
+          includeFood,
+          status: createdCharge.status.toUpperCase(),
+          paymentId: createdCharge.id,
+          userId: id,
+        },
+        { transaction: t }
+      );
+
+      // create bookingpets
+      petIds.map(async (petId) => {
+        const pet = await Pet.findOne({
+          where: { id: petId },
+          attributes: {
+            exclude: ["id", "createdAt", "updatedAt"],
+          },
+        });
+        if (pet) {
+          await Bookingpet.create(
+            { ...pet.dataValues, bookingId: booking.id },
+            { transaction: t }
+          );
+        } else {
+          createError("Pet not found");
+        }
+      });
+
+      // find house
+      const house = await House.findOne({
+        where: { id: houseId },
+        attributes: {
+          exclude: ["id", "createdAt", "updatedAt", "size", "other"],
+        },
+      });
+      if (!house) {
+        createError("House not found");
       }
-    );
+
+      // variable for receiving createdHost's id
+      let createdHost;
+      // create host
+      const host = await User.findOne({
+        where: { id: house.userId },
+        attributes: {
+          exclude: ["id", "createdAt", "updatedAt", "password", "email"],
+        },
+      });
+      if (host) {
+        createdHost = await Host.create(
+          {
+            ...host.dataValues,
+            bookingId: booking.id,
+            userId: house.userId,
+          },
+          { transaction: t }
+        );
+      } else {
+        createError("Host user not found");
+      }
+
+      // create customer
+      const customer = await User.findOne({
+        where: { id },
+        attributes: {
+          exclude: ["id", "createdAt", "updatedAt", "password", "email"],
+        },
+      });
+      if (customer) {
+        await Bookingcustomer.create(
+          {
+            ...customer.dataValues,
+            bookingId: booking.id,
+            userId: id,
+          },
+          { transaction: t }
+        );
+      } else {
+        createError("Customer user not found");
+      }
+
+      // create booking house
+      await Bookinghouse.create(
+        {
+          ...house.dataValues,
+          houseId,
+          bookingId: booking.id,
+          hostId: createdHost.id,
+        },
+        { transaction: t }
+      );
+
+      res.json({ booking });
+    });
   } catch (err) {
     next(err);
   }
