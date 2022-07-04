9a7fb39ba0fd0ff296b841a0081aacc4322d64d7
Mink <ntn.mink@gmail.com>
2022-07-04 16:30:08 +0700

    fixed dirname

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -5,7 +5,7 @@ const { v4: uuidv4 } = require("uuid");
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, path.join(__dirname, "/public/images"));
+    cb(null, path.join(__dirname, "../public/images"));
   },
   filename: (req, file, cb) => {
     cb(null, uuidv4() + "." + file.mimetype.split("/")[1]);
